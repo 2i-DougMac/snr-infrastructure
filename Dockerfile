0fb44d5be593ee8359d109e23ef95defebbdfa79
Douglas MacIntyre <douglas.macintyre@2itesting.com>
2025-02-11 13:03:09 +0000

    Added app and Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+# Specify the Base Image on which to build.
+# (FROM is the first command of a Dockerfile)
+FROM node:23-alpine3.20
+
+# Copy the package.json && package-lock.json
+# as well as the application directory with
+# '/' at the end of '/user/app' used so that Docker
+# will create the directory if it doesn't exist.
+COPY src /usr/app/
+COPY package.json /usr/app/
+COPY package-lock.json /usr/app/
+
+# WORKDIR is like cd - it changes the directory
+# from which the next command will be executed.
+WORKDIR /usr/app
+
+# Install the application dependencies.
+RUN npm install
+
+# Define the container startup command.
+# (CMD is the last command of a Dockerfile)
+CMD ["node", "server.js"]
---
+++ src/server.js
@@ -0,0 +1,57 @@
+// A Lovely Horse Application
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>A Lovely Horse Application</title>
+        <script>
+          const lines = [
+            "(My lovely, lovely, lovely horse)",
+            "My lovely horse (My lovely horse)",
+            "Running through the fields (Running through the fields)",
+            "Where are you going",
+            "With your fetlocks blowing",
+            "In the wind? (All summer)",
+            "I want to shower you with sugar lumps",
+            "And ride you over fences",
+            "Polish your hooves every single day",
+            "And bring you to the horse dentist",
+            "(My lovely, lovely, lovely horse)",
+            "My lovely horse (My lovely horse)",
+            "You're a pony no more (You're a pony no more)",
+            "Running around",
+            "With a man on your back (But...)",
+            "Like a train in the night, yeah (...I love you anyway)",
+            "Like a train in the night",
+            "(My lovely, lovely, lovely horse)"
+          ];
+
+          let currentLine = 0;
+          
+          // Add the next line from the array and increment the line
+          function showLine() {
+            if (currentLine < lines.length) {
+              const newLine = document.createElement('p');
+              newLine.innerText = lines[currentLine];
+              document.getElementById('lineContainer').appendChild(newLine);
+              currentLine++;
+            }
+          }
+        </script>
+      </head>
+      <body onclick="showLine()">
+        <h1>My Lovely Horse</h1>
+        <!-- Container where the lines will be appended -->
+        <div id="lineContainer"></div>
+      </body>
+    </html>
+  `);
+});
+
+app.listen(3000, function () {
+  console.log("app listening on port 3000");
+});
